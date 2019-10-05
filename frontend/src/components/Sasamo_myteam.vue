fc2ab5d4ee5997b2d6a4b24366c999e9b9195e2c
EdenKang <kg0116@ojworld.co.kr>
2019-10-05 14:00:40 +0900

    팀배정 완료

--- frontend/src/components/Sasamo_myteam.vue
+++ frontend/src/components/Sasamo_myteam.vue
@@ -6,17 +6,27 @@
             <tr>
                 <th>이름</th>
                 <th>소속팀</th>
-                <th>배정그룹</th>
-                <th>사역자유형</th>
-                <th>작업하기</th>
+                <th>현재 배정그룹</th>
+                <th>멤버유형</th>
+                <th>배정할 그룹</th>
             </tr>
             <tr v-for="(item, idx) in team" :key="item.ssm_id">
                 <td>{{ item.ssm_name }}</td>
                 <td>{{ item.ssm_team }}</td>
                 <td>{{ item.ssm_group ? item.ssm_group : '-'  }} </td>
                 <td>{{ item.ssm_type }}</td>
-                <td><input type="text" v-model="ssm_group"></td> 
-                <!-- <td><select option>-->
+                <td>
+                    <select :id="item.ssm_name" v-model="item.ssm_id" @change="onChange($event)">
+                        <option value="1">1팀</option>
+                        <option>2팀</option>
+                        <option>3팀</option>
+                        <option>4팀</option>
+                    </select>
+                </td> 
+                
+            </tr>
+            <tr>
+                <td> {{ exp }} </td>
             </tr>
             <tr>
                 <td colspan="5">
@@ -28,6 +38,7 @@
         </table>
     </form>
 </div>
+
         
 </template>
 <style>
@@ -54,22 +65,24 @@ export default {
       .then((result) => {
           let myTeam = result.data.data
           for(var i in myTeam){
-              console.log(myTeam[i].ssm_type)
+              this.editTeam.push({'id' : myTeam[i].ssm_id, 'ssm_group' : ''})
               switch (myTeam[i].ssm_type){
                   case 'I' : 
-                    myTeam[i].ssm_type = '훈련생'
+                    myTeam[i].ssm_type = 'A'
                     break
                   case 'P' : 
-                    myTeam[i].ssm_type = '예언사역자'
+                    myTeam[i].ssm_type = 'B'
                     break
                   case 'M' : 
-                    myTeam[i].ssm_type = '교역자'
+                    myTeam[i].ssm_type = 'C'
                     break
-                  default : myTeam[i].ssm_type = '치유사역자'
+                  default : myTeam[i].ssm_type = 'D'
               }
             console.log('TYPE :', myTeam[i].ssm_type, i)
+            console.log(this.editTeam[i])
           }
           this.team = myTeam
+          
           // console.log('LIST:', typeof this.team, this.team)
       })
       
@@ -79,14 +92,25 @@ export default {
     return {
       greeting : "아래는 사역을 신청한 사역자의 명단입니다. 팀을 배정해주세요",
       team: [],
+      editTeam: [],
+      exp: []
     }
   },
   methods: {
     goBack() {
-         this.$router.push({ name: 'sasamo_main'})
+         this.$router.push({ name: 'sasamo_main' })
+     },
+
+     onChange(event) {
+        this.exp.push({'id': event.target.id, 'ssm_group' : event.target.value })
+        
      }
+     
 
   }
+
+  
+
 
 }
 </script>
